4b70dfd1ca3ff521d7f6d91590889edb233a074f
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2024-07-19 15:53:32 -0300

    update July_2024

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -9,10 +9,10 @@ export const RESUME_DATA = {
   locationLink: "https://www.google.com/maps/place/Rio+de+Janeiro",
   about:
     "Full Stack Engineer with a dedication to crafting projects that deliver innovative user experiences, with extra attention to details.",
-  summary:
-    "With just over 1 year of experience as a Full Stack Engineer, I specialize in utilizing React, React Native, Django, and Java to develop comprehensive solutions. My expertise extends to both frontend and backend development, having been the sole frontend developer for a CDN project where I also collaborated on the backend. Additionally, I collaborated in designing and implementing a SAAS product. My diverse experience includes volunteer work, and I take pride in successfully bringing projects to fruitions.",
+  summary: 
+    "As a developer with 2 years of experience, I possess a strong skill set in HTML, CSS, Python, and TypeScript, as well as extensive knowledge of frameworks and libraries such as React, Next.js, Node.js, NestJS, Spring, React Native, Django, and Flask. I am also experienced with Tauri, Rust, Docker, Redis, Swagger, PostgreSQL, Tailwind, and more. \n In addition to development technologies, I have hands-on experience with Cloud technologies, specifically AWS. My expertise includes EC2, RDS, Aurora, ElastiCache, DynamoDB, Lambda, SQS, and Route53. I am continuously expanding my knowledge in this area as I study for the AWS Developer Certification (DVA-C02). I have a solid background in both web and mobile development. One of my key strengths as a developer is my ability to innovate. I am a critical and analytical thinker, capable of quickly identifying user needs and devising effective solutions. I am highly organized, adept at managing my time and priorities efficiently. I thrive in team environments but am equally comfortable working independently without direct supervision. Additionally, I am a clear and effective communicator, able to explain technical concepts in a straightforward and easy-to-understand manner. As a highly motivated and passionate individual, I am committed to continuous learning and improvement of my technical and communication skills. I believe this dedication helps me to be a more efficient and effective developer",
   avatarUrl: "https://github.com/XxWhitepegasusxX.png",
-  personalWebsiteUrl: "https://alexanderalmeida.vercel.app",
+  personalWebsiteUrl: "https://alexanderalmeida-cv.vercel.app",
   contact: {
     email: "alexanderalmeida3040@gmail.com",
     tel: "+55022998035875",
@@ -44,12 +44,41 @@ export const RESUME_DATA = {
   ],
   work: [
     {
-      company: "NaluiStore",
-      link: "",
-      badges: ["Freelancer"],
+      company: "PDCA ED Tech",
+      link: "#",
+      badges: [],
+      title: "Developer | Help Desk",
+      start: 'May 2024',
+      end: 'Present',
+      description: [
+        "Managed employee documents across varioussystems, ensuring organization and accessibility.",
+        "Provided technicalsupport to numerous colleagues and clients, resolving issues efficiently and enhancing user experience",
+        "Developed automation solutions using Python and toolssuch as Selenium,streamlining workflows and increasing productivity for team members.",
+        "Created and maintained spreadsheetsto track document expiration dates, proactively preventing overdue issues and ensuring compliance."
+      ]
+    },
+    {
+
+      company: "Collaborator Manager",
+      link: "#",
+      badges: ["Freelancer", "Project"],
       title: "Mobile Developer",
-      start: 'Nov 2023',
-      end: 'Jan 2023',
+      start: '',
+      end: '',
+      description: [
+        "Utilized Next.js and TypeScript for the front-end development to ensure a robust and scalable interface.",
+        "Employed Tailwind CSS and ShadCN to create a responsive and visually appealing design.",
+        "Implemented Tauri with Rust on the back-end for efficient and secure operations.",
+        "Used SeaORM to build a SQLite database, ensuring reliable and organized data storage"
+      ]
+    },
+    {
+      company: "NaluiStore",
+      link: "#",
+      badges: ["Freelancer", "Project"],
+      title: "Mobile Developer",
+      start: '',
+      end: '',
       description: [
         "I designed and implemented a inventory management application for toy rentals. Using React Native and TypeScript, I ensured a cross-platform, performant user interface. The application's data persistence was optimized using WatermelonDB, enhancing offline capabilities and responsiveness. This project showcased my proficiency in mobile development, TypeScript, and efficient data management techniques",
         "I implemented Docker into the project,streamlining the development environment and enhancing deployment consistency. This addition optimizes the project's workflow, ensuring efficient collaboration and scalability.",
@@ -58,22 +87,22 @@ export const RESUME_DATA = {
     },
     {
       company: "Plin Tech",
-      link: "https://plin.app/furando-a-fila",
+      link: "#",
       badges: ['Temporary', 'Freelancer'],
       title: "Full Stack Developer",
       start: "Sep 2023",
       end: "Oct 2023",
-      description:
-        ["Designed and developed an admin dashboard responsible for managing content and the client's visual identity. For the front-end, I employed React, Next.js, and Tailwind, and for the back-end, I utilized NestJS, all implemented in TypeScript.",
-         "Worked collaboratively with a team to craft a responsive, user-centric interface and build a scalable, secure API.",
-         "Successfully incorporated a wide range of features, spanning login, checkout, user profiles, and registration, to enhance overall functionality and user experience.",
-         "I created the client interface, adhering to industry best practices and design standards. Utilizing Tailwind CSS and Next.js with TypeScript, I ensured a responsive and visually appealing user experience, with a focus on accessibility, performance, and maintainability."
-        ],
+      description: [
+        "Designed and developed an admin dashboard responsible for managing content and the client's visual identity. For the front-end, I employed React, Next.js, and Tailwind, and for the back-end, I utilized NestJS, all implemented in TypeScript.",
+        "Worked collaboratively with a team to craft a responsive, user-centric interface and build a scalable, secure API.",
+        "Successfully incorporated a wide range of features, spanning login, checkout, user profiles, and registration, to enhance overall functionality and user experience.",
+        "I created the client interface, adhering to industry best practices and design standards. Utilizing Tailwind CSS and Next.js with TypeScript, I ensured a responsive and visually appealing user experience, with a focus on accessibility, performance, and maintainability."
+      ],
     },
     {
       company: "Configr",
       link: "https://configr.com/",
-      badges: [],
+      badges: ["Full-Time"],
       title: "Full Stack Developer",
       start: "Apr 2023",
       end: "Sep 2023",
@@ -90,18 +119,18 @@ export const RESUME_DATA = {
     {
       company: "Reevo",
       link: "#",
-      badges: [],
+      badges: ["Full-Time"],
       title: "IT Assistant | Developer",
       start: "Jan 2023",
       end: "Mar 2023",
       description:
-        [`Developed and maintained modern, feature-rich company website using React, Next.js, and TypeScript, integrating animations and third-party services`,
+        ["Developed and maintained modern, feature-rich company website using React, Next.js, and TypeScript, integrating animations and third-party services",
          "Managed client quotations, budgets, contracts, and documents, ensuring organization, security, and legal compliance."
         ],
     },
     {
       company: "Brinquedos para todos",
-      link: "https://www.nokia.com",
+      link: "#",
       badges: ["Volunteer"],
       title: "Frontend Developer",
       start: "Jan 2023",
@@ -110,7 +139,7 @@ export const RESUME_DATA = {
     },
     {
       company: "Girl Now Woman Later",
-      link: "https://www.nokia.com",
+      link: "#",
       badges: ["Volunteer"],
       title: "Frontend Developer",
       start: "Oct 2022",
@@ -133,7 +162,9 @@ export const RESUME_DATA = {
     "Redis/RabbitMQ",
     "CI/CD",
     "Microservices Architecture",
-    "Algorithms"
+    "Algorithms",
+    "Rust",
+    "Tauri/Electron"
   ],
   projects: [
     {
